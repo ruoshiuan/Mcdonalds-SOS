fcd3e84e1d698599a67d8e0df17da168999c3335
ruoshiuan <ruoshiuan.wu@gmail.com>
2021-08-03 14:04:40 +0800

    order, complete頁面RWD

--- src/pages/Complete/CompletePage.js
+++ src/pages/Complete/CompletePage.js
@@ -1,11 +1,11 @@
 import React,{ useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom'
+import { useHistory, Link } from 'react-router-dom'
 import './completepage.css'
 import Navbar from '../../components/Navbar'
 import Footer from '../../components/Footer'
 import takein from '../../images/takein.svg'
 import takeout from '../../images/takeout.svg'
-import { Link } from 'react-router-dom'
+import completeImage from '../../images/completeImage.jpg'
 
 const CompletePage = () => {
   const [loading, setLoading] = useState(true)
@@ -19,35 +19,40 @@ const CompletePage = () => {
     return () => setLoading(false)
   }, [loading])
   return (
-    <>
-      <Navbar />
-      <main>
-        {record ?
-        <div className="content">
-          <div className="thanksCorner">
-            <div className="thankyou">Thank you!</div>
-            <div style={{ fontSize:'22px' }}>感謝您使用自助點餐服務</div>
-            <div style={{ fontSize:'20px', paddingTop: '15px' }}>取餐號碼</div>
-            <div className="orderNumber">{ record.orderNumber }</div>
+  <>
+    <Navbar />
+    <main>
+      {record ?
+      <div className="content">
+        <div className="thanksCorner">
+          <div className="thankyou">Thank you!</div>
+          <div style={{ fontSize:'22px' }}>感謝您使用自助點餐服務</div>
+          <div style={{ fontSize:'20px', paddingTop: '15px' }}>取餐號碼</div>
+          <div className="orderNumber">{ record.orderNumber }</div>
+        </div>
+        <div className="thanksCornerAlert">
+          <div>餐點製作中</div>
+          <div>請於櫃台上方螢幕顯示此號碼時前來取餐</div>
+        </div>
+        <div className="receiptCorner">
+          <div className="cornerLeft">
+            <img src={ completeImage } alt="photo" className="completeImage"/>
           </div>
-          <div className="thanksCornerAlert">
-            <div>餐點製作中</div>
-            <div>請於櫃台上方螢幕顯示此號碼時前來取餐</div>
-          </div>
-          <div className="receiptCorner">
+          <div className="cornerRight">
             <div className="picBg">
-            { record.mealType === '內用' ? <img className="takePic" src={takein} alt="takein" /> : <img className="takePic" src={takeout} alt="takeout" />}
+            { record.mealType === '內用' ? <img className="takePic" src={takein} alt="takein" /> : <img className="takePic" src={takeout} alt="takeout" /> }
             </div>
-            <div className="dinnerType">用餐方式 - <span>{ record.mealType }</span></div>
+            <div className="dinnerType">用餐方式 - <span style={{ backgroundColor: '#fcecbd', padding: '0 3px' }}>{ record.mealType }</span></div>
             <strong>店家資訊</strong>
             <div>{ record.store }店</div>
             <div>{ record.address }</div>
-            </div>
-            <Link to="/"><button className="backToHome">返回主頁</button></Link>
-        </div> : null }
-      </main>
-      <Footer />
-    </>
+          </div>
+        </div>
+          <Link to="/"><button className="backToHome">返回主頁</button></Link>
+      </div> : null }
+    </main>
+    <Footer />
+  </>
   )
 }
 
--- src/pages/Order/OrderPage.js
+++ src/pages/Order/OrderPage.js
@@ -47,25 +47,24 @@ const OrderPage = () => {
       mealType: mealWay,
       store: storeInfo.store,
       address: storeInfo.address,
-
     }
-      sessionStorage.setItem('orderRecord', JSON.stringify(data))
-      localStorage.setItem('cartItems',JSON.stringify([]))
-      history.push('/complete')
+    sessionStorage.setItem('orderRecord', JSON.stringify(data))
+    localStorage.setItem('cartItems',JSON.stringify([]))
+    history.push('/complete')
     }
   }
   const getTotal = getCartInfo.reduce((total, order) => {
     return total + order.total
   }, 0)
   const orderInfo = getCartInfo.map(info => {
-      return (
-          <div className="orderInfo" key={ info.id }>
-            <div className="orderMainTitle"><span>{ info.meal }</span><span className="orderQuantity">x{ info.quantity }</span></div>
-            {info.side || info.drink === null ? <div className="mealSide">{ info.side }, { info.drink }</div> : null }
-            <div className="orderMealTotal">$ { info.total }</div>
-        </div>
-      )
-    })
+    return (
+    <div className="orderInfo" key={ info.id }>
+      <div className="orderMainTitle"><span>{ info.meal }</span><span className="orderQuantity">x{ info.quantity }</span></div>
+      {info.side || info.drink === null ? <div className="mealSide">{ info.side }, { info.drink }</div> : null }
+      <div className="orderMealTotal">$ { info.total }</div>
+    </div>
+    )
+  })
   return (
     <>
       <Navbar />
@@ -74,41 +73,43 @@ const OrderPage = () => {
           {storeInfo ? <h2>在 { storeInfo.store } 取餐</h2> : null}
           {storeInfo ? <div>{ storeInfo.address }</div> : null}
         </div>
-          <form className="orderForm" onSubmit={ submitOrder }>
-              <h3>選擇用餐方式</h3>
-              <div className="oneOption">
-                <input type="radio" className="radioStyle" id="takeout" name="diningTypes" value="外帶" onChange={(e) => setMealWay(e.target.value)}/>
-                <label htmlFor="takeout" className="labelStyle">外帶<span style={{ color: '#C50406', fontSize: '14px' }}>　得來速不得使用自助點餐</span></label>
-              </div>
-              <div className="oneOption">
-                <input type="radio" className="radioStyle" id="takein" name="diningTypes" value="內用" onChange={(e) => setMealWay(e.target.value)}/>
-                <label htmlFor="takein" className="labelStyle">內用</label>
-              </div>
-              <h3>選擇付款方式</h3>
-              <div className="oneOption">
-                <input type="radio" className="radioStyle" id="line" name="payWays" value="LinePay" onChange={(e)=>setPayWay(e.target.value)} />
-                <label htmlFor="line" className="labelStyle">Line Pay</label>
-              </div>
-              <div className="oneOption">
-                <input type="radio" className="radioStyle" id="apple" name="payWays" value="ApplePay" onChange={(e)=>setPayWay(e.target.value)} />
-                <label htmlFor="apple" className="labelStyle">Apple Pay</label>
-              </div>
-              <div className="oneOption">
-                <input type="radio" className="radioStyle" id="jko" name="payWays" value="街口支付" onChange={(e)=>setPayWay(e.target.value)} />
-                <label htmlFor="jko" className="labelStyle">街口支付</label>
-              </div>
-              <div className="oneOption">
-                <input type="radio" className="radioStyle" id="credit" name="payWays" value="信用卡" onChange={(e)=>setPayWay(e.target.value)} />
-                <label htmlFor="credit" className="labelStyle">信用卡</label>
-              </div>
-              <h3>餐點最終確認</h3>
-              <div className="orderCart">
-                { orderInfo }
+          <form className="orderForm">
+            <h3>選擇用餐方式</h3>
+            <div className="oneOption">
+              <input type="radio" className="radioStyle" id="takeout" name="diningTypes" value="外帶" onChange={(e) => setMealWay(e.target.value)}/>
+              <label htmlFor="takeout" className="labelStyle">外帶<span style={{ color: '#C50406', fontSize: '14px' }}>　得來速不得使用自助點餐</span></label>
             </div>
-            <h2>合計 $<span>{ getTotal }</span></h2>
+            <div className="oneOption">
+              <input type="radio" className="radioStyle" id="takein" name="diningTypes" value="內用" onChange={(e) => setMealWay(e.target.value)}/>
+              <label htmlFor="takein" className="labelStyle">內用</label>
+            </div>
+            <h3>選擇付款方式</h3>
+            <div className="oneOption">
+              <input type="radio" className="radioStyle" id="line" name="payWays" value="LinePay" onChange={(e)=>setPayWay(e.target.value)} />
+              <label htmlFor="line" className="labelStyle">Line Pay</label>
+            </div>
+            <div className="oneOption">
+              <input type="radio" className="radioStyle" id="apple" name="payWays" value="ApplePay" onChange={(e)=>setPayWay(e.target.value)} />
+              <label htmlFor="apple" className="labelStyle">Apple Pay</label>
+            </div>
+            <div className="oneOption">
+              <input type="radio" className="radioStyle" id="jko" name="payWays" value="街口支付" onChange={(e)=>setPayWay(e.target.value)} />
+              <label htmlFor="jko" className="labelStyle">街口支付</label>
+            </div>
+            <div className="oneOption">
+              <input type="radio" className="radioStyle" id="credit" name="payWays" value="信用卡" onChange={(e)=>setPayWay(e.target.value)} />
+              <label htmlFor="credit" className="labelStyle">信用卡</label>
+            </div>
+            <h3>餐點最終確認</h3>
+            <div className="orderCart">
+              { orderInfo }
+            </div>
+            <div className="orderTotal">合計 $<span>{ getTotal }</span></div>
           </form>
-          <button className="backToCartBtn" onClick={() => {history.push('/menu')} }>回上一頁</button>
-          <button className="orderBtn">確定結帳</button>
+          <div className="orderBottomBtn">
+            <button className="backToCartBtn" onClick={() => {history.push('/menu')} }>回上一頁</button>
+            <button className="orderBtn" onClick={ submitOrder }>確定結帳</button>
+          </div>
       </main>
       <Footer />
     </>
